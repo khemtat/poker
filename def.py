b8b376088d37eec5411469c0745425d3e6fa7a09
d3viz <s6070009@kmitl.ac.th>
2013-10-04 13:34:26 +0700

    test

--- def.py
+++ def.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def gg(n):
 	print 'Hello World'*n
 
@@ -9,6 +8,3 @@ print 'PAme'
 
 print 'Test sokaopfksopfksopfkeopgkrhkopgkr;lgkr;ogr;kf gres; gjesr;g jer;pg jerp gj4pjp j4p'
 print 'Yok kak kak'
-
-=======
->>>>>>> 02115b1d818a187ed0698095b24669b1523b12ef
