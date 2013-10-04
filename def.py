ed3268284a807c32319255e27f324dfb0b9e0d9b
d3viz <s6070009@kmitl.ac.th>
2013-10-04 13:31:16 +0700

    sync branch

--- def.py
+++ def.py
@@ -5,7 +5,7 @@ print gg(input())
 
 print 'YEAH'
 print 'PAme'
-<<<<<<< HEAD
+
 print 'Test sokaopfksopfksopfkeopgkrhkopgkr;lgkr;ogr;kf gres; gjesr;g jer;pg jerp gj4pjp j4p'
 print 'Yok kak kak'
->>>>>>> cced989aef5ff6c219f668f1619b9ea89aac52d7
+
