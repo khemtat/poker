eba1d107d9bf57ceac7ffc7d67b327958a1c796f
d3viz <s6070009@kmitl.ac.th>
2013-10-04 17:12:33 +0700

    Add a new code

---
+++ poker_test.py
@@ -0,0 +1,13 @@
+import unittest
+import poker
+class TestPoker(unittest.TestCase):
+    '''Example unittest test methods for #function#.'''
+    def test_divisors_example_1(self):
+        '''Test #function# poker with #something#.'''
+        actual = poker.#Function#
+        expected = something##
+        self.assertEqual(actual, expected)
+     
+if __name__ == '__main__':
+    unittest.main(exit=False)
+
