b14b0c228858c0289251e0194951fe052e4595a8
kittikorn <s6070006@kmitl.ac.th>
2013-10-05 16:52:33 +0700

    Edit edit card_list in def play_poker

--- poker.py
+++ poker.py
@@ -1,9 +1,8 @@
 def play_poker(n):
 	card_list = []
 	for i in xrange(n):
-		card_list.append(input("[+] Player "+str(i+1)+" input your card: "))
-	return card_list
-
+		card_list.append([raw_input("[+] Player "+str(i+1)+" input your card: ")])
+		return card_list
 if __name__ == '__main__':
 	import doctest
-	doctest.testmod() 	
+	doctest.testmod()
