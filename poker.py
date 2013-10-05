6800218cf304fc177ec7947aad1c004f5f8d9047
kittikorn <s6070006@kmitl.ac.th>
2013-10-05 12:58:36 +0700

    Add insert player & card

--- poker.py
+++ poker.py
@@ -1,1 +1,5 @@
-player = input('Player :')
+def poker_main():
+    player = input('Enter number of player : ')
+    for i in xrange(0,player):
+        raw_input("Player "+str(i+1)+" input your card : ")
+poker_main()
