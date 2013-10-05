23b3e1c094d7c6a7d1b82141d52f7c6660946677
kittikorn <s6070006@kmitl.ac.th>
2013-10-05 22:51:12 +0700

    Edit test case and play_poker
    Edit syntax error in play_poker and all sematic error test case

--- poker.py
+++ poker.py
@@ -2,7 +2,7 @@ def play_poker(n):
 	card_list = []
 	for i in xrange(n):
 		card_list.append([raw_input("[+] Player "+str(i+1)+" input your card: ")])
-		return card_list
+	return card_list
 def flush(hand):
         """
         (hand) --> bool
--- poker_test.py
+++ poker_test.py
@@ -5,17 +5,17 @@ class TestPoker(unittest.TestCase):
     def test_poker_example_1(self):
         '''Test check_rank poker with .'''
         actual = poker.play_poker(1)
-        expected = [1]
+        expected = [['1']]
         self.assertEqual(actual, expected)
     def test_poker_flush_1(self):
         '''Test Case Flush 1 '''
         actual = poker.flush(['JC', 'TC', '9C', '8C', '7C'])
-        expected = [True]
+        expected = True
         self.assertEqual(actual, expected)
     def test_poker_flush_2(self):
         '''Test Case Flush 2 '''
         actual = poker.flush(['JD', 'TC', '9C', '8C', '7C'])
-        expected = [False]
+        expected = False
         self.assertEqual(actual, expected)
      
 if __name__ == '__main__':
