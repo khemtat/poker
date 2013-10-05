23b74cab58e6b39e32fa1ea06f8377725f1cb643
kittikorn <s6070006@kmitl.ac.th>
2013-10-05 22:23:38 +0700

    Added flush check function

--- poker.py
+++ poker.py
@@ -3,6 +3,19 @@ def play_poker(n):
 	for i in xrange(n):
 		card_list.append([raw_input("[+] Player "+str(i+1)+" input your card: ")])
 		return card_list
+def flush(hand):
+        """
+        (hand) --> bool
+
+        return check this hand is flush
+        """
+        s = [h for n,h in hand]
+        status = 1
+        for i in xrange(len(s)):
+                if s[0] != s[i]:
+                        status = 0
+                        break
+        return bool(status)
 if __name__ == '__main__':
 	import doctest
 	doctest.testmod()
