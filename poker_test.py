fa8f7bbfec5ca07fa4673eaadaf56dc0d5fad6a6
kittikorn <s6070006@kmitl.ac.th>
2013-10-05 22:23:22 +0700

    test case flush check function

--- poker_test.py
+++ poker_test.py
@@ -7,6 +7,16 @@ class TestPoker(unittest.TestCase):
         actual = poker.play_poker(1)
         expected = [1]
         self.assertEqual(actual, expected)
+    def test_poker_flush_1(self):
+        '''Test Case Flush 1 '''
+        actual = poker.flush(['JC', 'TC', '9C', '8C', '7C'])
+        expected = [True]
+        self.assertEqual(actual, expected)
+    def test_poker_flush_2(self):
+        '''Test Case Flush 2 '''
+        actual = poker.flush(['JD', 'TC', '9C', '8C', '7C'])
+        expected = [False]
+        self.assertEqual(actual, expected)
      
 if __name__ == '__main__':
     unittest.main(exit=False)
