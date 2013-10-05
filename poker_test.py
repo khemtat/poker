3274599874dbc6220ca7e20aef7affc1a5a65bf9
d3viz <s6070009@kmitl.ac.th>
2013-10-05 15:10:57 +0700

    Update code in poker_test.py

--- poker_test.py
+++ poker_test.py
@@ -1,11 +1,11 @@
 import unittest
 import poker
 class TestPoker(unittest.TestCase):
-    '''Example unittest test methods for #function#.'''
-    def test_divisors_example_1(self):
-        '''Test #function# poker with #something#.'''
-        actual = poker.#Function#
-        expected = something##
+    '''Example unittest test methods for check_rank'''
+    def test_poker_example_1(self):
+        '''Test check_rank poker with .'''
+        actual = poker.play_poker(1)
+        expected = [[1]]
         self.assertEqual(actual, expected)
      
 if __name__ == '__main__':
