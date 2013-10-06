df0eb966923fdac99b1a17fbd55d0bc9622a33f1
d3viz <s6070009@kmitl.ac.th>
2013-10-06 13:15:30 +0700

    MOVE 'straight' to line 23

--- poker.py
+++ poker.py
@@ -24,15 +24,14 @@ def straight(hand):
         """
         (hand) -> bool
 
-        checking hand is straight
-
-        return True or False
+        return check this hand is straight
         """
-        s = ['--23456789TJQKA'.index(s1) for s1,s2 in hand]
-        s.sort()
-        if s == [2, 3, 4, 5, 14]:
-                s = [1, 2, 3, 4, 5]
-        return max(s)-min(s) == 4 and len(set(s)) == 5
+        tocheck_straight = ['--23456789TJQKA'.index(n) for n,h in hand]
+        tocheck_straight.sort()
+        tocheck_straight.reverse()
+        if tocheck_straight == [14,5,4,3,2]:
+                tocheck_straight = [5,4,3,2,1]
+        return (max(tocheck_straight)-min(tocheck_straight) == 4) and (len(set(tocheck_straight)) == 5)
 
 
 def flush(hand):
@@ -45,6 +44,7 @@ def flush(hand):
         if len(set(s)) != 1:
                 return False
         return True
+        
 def three_of_a_kind(hand):
         """
         (hand) -> bool
@@ -59,19 +59,7 @@ def three_of_a_kind(hand):
                         status = 1
                         break
         return bool(status)
-def straight(hand):
-        """
-        (hand) -> bool
 
-        return check this hand is straight
-        """
-        tocheck_straight = ['--23456789TJQKA'.index(n) for n,h in hand]
-        tocheck_straight.sort()
-        tocheck_straight.reverse()
-        if tocheck_straight == [14,5,4,3,2]:
-                tocheck_straight = [5,4,3,2,1]
-        return (max(tocheck_straight)-min(tocheck_straight) == 4) and (len(set(tocheck_straight)) == 5)
-        
 if __name__ == '__main__':
 	import doctest
 	doctest.testmod()
